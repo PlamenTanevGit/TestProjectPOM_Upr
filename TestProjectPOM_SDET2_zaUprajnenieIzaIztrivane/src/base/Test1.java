ee7cdab0853b43d663b856733948c367381ea833
PlamenTanev <PlamenTanev@2210-BGR>
2020-02-02 22:51:05 +0200

    2nd Commit

--- TestProjectPOM_SDET2_zaUprajnenieIzaIztrivane/src/base/Test1.java
+++ TestProjectPOM_SDET2_zaUprajnenieIzaIztrivane/src/base/Test1.java
@@ -1,7 +1,14 @@
 package base;
 
 
-import org.openqa.selenium.By;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.Test;
 
 public class Test1 extends base{
@@ -10,16 +17,39 @@ public class Test1 extends base{
 	
 	
 	@Test
-	public void t1 () {
+	public void t1 () throws IOException {
 		
-		System.out.println("1");
+		System.out.println("Test 1");
 		//driver.get("https://www.way2automaiton.com/angularjs-protractor/banking/#/manager");
 		
-		driver.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
+		super.get("http://www.way2automation.com/angularjs-protractor/banking/#/login");
 		//String browser_name = System.getenv("BROWSER_NAME");
 		//System.out.println(browser_name);
-		driver.findElement(By.xpath(or.getProperty("BankManagerLogin_btn_XPATH"))).click();
 		
+		double height = super.getHeight(findElement("xpath", or.getProperty("BankManagerLogin_btn_XPATH")));
+		double width = super.getWidth(findElement("xpath", or.getProperty("BankManagerLogin_btn_XPATH")));
+		String webElmentTxt = findElement("xpath", or.getProperty("BankManagerLogin_btn_XPATH")).getText();
+		System.out.println(webElmentTxt + "  Height is : " +height + ",  Width is :  " + width);
+		
+		super.verifyDimentions(findElement("xpath", or.getProperty("BankManagerLogin_btn_XPATH")), 46.0, 201.0);
+		
+		System.out.println(" JavaScript Body is :  " +super.getJavaScriptBody());
+		
+		//super.findElement("xpath", or.getProperty("BankManagerLogin_btn_XPATH")).click();
+		//driver.findElement(By.xpath(or.getProperty("BankManagerLogin_btn_XPATH"))).click();
+		  
+		
+		
+	}
+	
+	
+	
+	final public BufferedImage getScreenshot() throws IOException {
+		if (!(driver instanceof TakesScreenshot))
+			throw new IOException("This browser driver does not support screenshot");
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		byte[] data = takesScreenshot.getScreenshotAs(OutputType.BYTES);
+		return ImageIO.read(new ByteArrayInputStream(data));
 	}
 	
 }
--- TestProjectPOM_SDET2_zaUprajnenieIzaIztrivane/src/base/base.java
+++ TestProjectPOM_SDET2_zaUprajnenieIzaIztrivane/src/base/base.java
@@ -6,9 +6,12 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -37,23 +40,22 @@ public class base {
 	public static Logger Log = Logger.getLogger(base.class);
 	public static WebDriverWait wait;
 	public ExtentReports extentTest;
-	
+
 	public static double startTime;
-	public static double endTime; 
-	public static double duration ;
-	
+	public static double endTime;
+	public static double duration;
+
 	@BeforeSuite
 	public void setUp() {
-		
+
 		/*
-		 * Chrome options - set the browser optiosn
-		 * Desired Capabilities - accept the Oprtions as parameter
-		 * Driver - accept the options 
+		 * Chrome options - set the browser optiosn Desired Capabilities - accept the
+		 * Oprtions as parameter Driver - accept the options
 		 */
+		
 		ChromeOptions options = new ChromeOptions();
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		
-		
+
 		startTime = System.nanoTime();
 		if (driver == null) {
 			String orFile = System.getProperty("user.dir") + "\\or.properties";
@@ -93,82 +95,186 @@ public class base {
 //				prefs.put("profile.default_content_setting_values.notifications", 2);
 //				prefs.put("credentials_enable_service", false);
 //				prefs.put("profile.password_manager_enabled", false);
-				
-				
-				//options.setExperimentalOption("prefs", prefs);
+
+				// options.setExperimentalOption("prefs", prefs);
 				options.addArguments("--disable-extensions");
 				options.addArguments("--disable-infobars");
-				//options.addArguments("--headless");
-				
+				// options.addArguments("--headless");
+
 				desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
 				LoggingPreferences logPrefs = new LoggingPreferences();
-			    logPrefs.enable(LogType.BROWSER, Level.ALL);
-			    desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
-			    desiredCapabilities.setBrowserName(or.getProperty("browser"));
-			    desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-			    desiredCapabilities.setCapability(CapabilityType.PLATFORM_NAME, "Windows 10");
-			    desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, or.getProperty("browser"));
-			    desiredCapabilities.setCapability(CapabilityType.VERSION, or.getProperty("80"));
-			    /*
-				 *  Merging DesiredCapabilities for chrome
+				logPrefs.enable(LogType.BROWSER, Level.ALL);
+				desiredCapabilities.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+				desiredCapabilities.setBrowserName(or.getProperty("browser"));
+				desiredCapabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+				desiredCapabilities.setCapability(CapabilityType.PLATFORM_NAME, "Windows 10");
+				desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, or.getProperty("browser"));
+				desiredCapabilities.setCapability(CapabilityType.VERSION, or.getProperty("80"));
+				System.out.println("browser name : "+desiredCapabilities.getBrowserName());
+				System.out.println("browser version : " +desiredCapabilities.getVersion());
+				System.out.println("Platform is  : " +desiredCapabilities.getPlatform());
+				/*
+				 * Merging DesiredCapabilities for chrome
 				 */
 				options.merge(desiredCapabilities);
-				
+
 				driver = new ChromeDriver(options);
-				
-				 
 
 			}
-			
+
 			else if (or.getProperty("browser").equalsIgnoreCase("firefox")) {
 				browser = "firefox";
 				System.setProperty("webdriver.chrome.driver",
-				System.getProperty("user.dir") + "\\Drivers\\geckodriver.exe");
+						System.getProperty("user.dir") + "\\Drivers\\geckodriver.exe");
 				driver = new FirefoxDriver();
-				
+
 			}
-			
-			
-//			switch (browser) {
-//			case "chrome":
-//				desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
-//				break;
-//
-//			case "firefox":
-//				desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, "Firefox");
-//				break;
-//				
-//				
-//			default:
-//				break;
-//			}
-		    
-		    
-		    
-		    
-		    
-		    driver.manage().window().maximize();
-		    
-		     
-			
+
+			driver.manage().window().maximize();
+			// parsing the Int to the property and making int
+			driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicitWait")),
+					TimeUnit.SECONDS);
+
 		}
+	}
+
+	final public void get(String sUrl) {
+		driver.get(sUrl);
+	}
+
+	final public String pageSource() {
+		return driver.getPageSource();
+	}
+	
+
+	public String getPageTitle () {
+		return driver.getTitle();
+	}
+	
+	public String getSourceCode (String Url) {
+		get(Url);
+		return driver.getPageSource();
+	} 
+	
+	public void closeWindow() {
+		driver.close();
+	}
+
+	public String getCurrentUrl() {
+		return driver.getCurrentUrl();
+	}
+
+	public WebElement getElement(String locatorType, String locatorPath) {
+		locatorType = locatorType.toLowerCase();
+
+		if (locatorType.equalsIgnoreCase("id")) {
+			return this.driver.findElement(By.id(locatorPath));
+		}
+		else if (locatorType.equals("name")) {
+			//System.out.println("Element found with Name locator: " + locatorPath);
+			return this.driver.findElement(By.name(locatorPath));
+
+		} else if (locatorType.equals("xpath")) {
+			//System.out.println("Elemen found with Xpath :" + locatorPath);
+			return this.driver.findElement(By.xpath(locatorPath));
+
+		} else if (locatorType.equals("css")) {
+			//System.out.println("Elemen found with CSS :" + locatorPath);
+			return this.driver.findElement(By.cssSelector(locatorPath));
+		} else if (locatorType.equals("classname")) {
+			//System.out.println("Element found with classname: " + locatorPath);
+			return this.driver.findElement(By.className(locatorPath));
+		} else if (locatorType.equals("tagname")) {
+			System.out.println("Element found with tagname: " + locatorPath);
+			return this.driver.findElement(By.tagName(locatorPath));
+		} else if (locatorType.equals("linktext")) {
+			System.out.println("Element found with link text: " + locatorPath);
+			return this.driver.findElement(By.linkText(locatorPath));
+		} else if (locatorType.equals("partiallinktext")) {
+			System.out.println("Element found with partial link text: " + locatorPath);
+			return this.driver.findElement(By.partialLinkText(locatorPath));
+		} else {
+			System.out.println("Locator type not supported");
+			return null;
+		}
+
+	}
+	
+	
+	public WebElement findElement (String locatorType, String locatorPath) {
+		try {
+			element = getElement(locatorType, locatorPath);
+			element.isDisplayed();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return element;
+		
+	}
+	
+	public double getHeight (WebElement element) {
+		return element.getSize().getHeight();
+		
+	}
+	
+	public double getWidth (WebElement element) {
+		return element.getSize().getWidth();
+		
+	} 
+	 
+	final public String getJavaScriptBody () {
+		try {
+			element = findElement("tagname", "body");
+			String javaScriptBody = element.getText();
+			return javaScriptBody;
+		//	return driver.findElement(By.tagName("body")).getText();    -- we can use this way of return as well
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+		
+	}
+	
+	public void verifyDimentions (WebElement element, double expectedHeight, double expectedWidth) {
+		Assert.assertEquals(expectedHeight, getHeight(element), 0.001);
+		Assert.assertEquals(expectedWidth, getWidth(element), 0.001);
 	}
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 	@AfterSuite(alwaysRun = true)
 	public static void jdiTearDown() throws IOException {
 		endTime = System.nanoTime();
-		duration = startTime - endTime  ; 
-		System.out.println(" Total Run Time :  "  + duration/ 1000000000.0 + " sec. ");
-		
-		
-		
-		if (driver!= null) {
-			driver.quit();
+		duration = startTime - endTime;
+		System.out.println(" Total Run Time :  " + duration / 1000000000.0 + " sec. ");
+
+		try {
+			Thread.sleep(5000);
+			if (driver!= null) {
+				//driver.quit();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+
 	}
-	
 
 }
